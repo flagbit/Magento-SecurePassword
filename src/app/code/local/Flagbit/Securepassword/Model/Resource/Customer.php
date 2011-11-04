4f42d9b63855e2776307474d6901222ce62dc093
Sven Kraenkel <kraenkel@flagbit.de>
2011-11-04 09:25:28 +0100

    [+TASK] edit the Resource model which extends the class from the entity model

---
+++ src/app/code/local/Flagbit/Securepassword/Model/Resource/Customer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 
+ * Customer entity model
+ * @category   Flagbit
+ * @package    Flagbit_Securepassword
+ * @author 	   Flagbit GmbH & Co. KG <magento@flagbit.de>
+ *
+ */
+class Flagbit_Securepassword_Model_Resource_Customer extends Flagbit_Securepassword_Model_Entity_Customer
+{
+	
+}
